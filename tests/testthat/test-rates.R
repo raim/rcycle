be0ee208501b5ad08a97f78b55d56f14e3c410ce
raim <rainer.machne@gmail.com>
2025-12-19 17:14:51 +0100

    added fake test for get_rates vs. get_times

---
+++ tests/testthat/test-rates.R
@@ -0,0 +1,12 @@
+#!/usr/bin/Rscript --vanilla
+
+library(rcycle)
+library(testthat)
+
+## TODO
+## * test get_rates, and recovery of times by get_times,
+
+## failing test until implemented
+test_that("are rates vs. times test implemented yet", {
+        expect_equal(0, 1, tolerance = 1e-10)
+    })
