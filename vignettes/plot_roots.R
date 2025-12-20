f216fba3c70875968b7e6daf19ce7914aac44554
raim <rainer.machne@gmail.com>
2025-12-20 17:52:10 +0100

    added script to test root finding functions

---
+++ vignettes/plot_roots.R
@@ -0,0 +1,41 @@
+library(rcycle)
+
+
+#. temp requirements
+source('/home/raim/programs/rcycle/R/models.R')
+
+## Testin root finding functions
+
+## TODO:
+## * understand all root finding functions, and find
+##   best solution, ideally just stats::uniroot,
+##   or informed selection from uniroot.all.
+
+
+a <- 1.32991318770238
+gamma <- 0.411938922118397
+phi <- 0.5
+A <- 2.44109012400914e-05
+k <- 3.5480961104784e-05
+
+x <- seq(-10,10,.01)
+tau_k <- root_tau_k(x, a = a, gamma = gamma, phi = phi, A = NA, k = NA)
+roots_k <- rootSolve::uniroot.all(f=root_tau_k,
+                                  a = a, gamma = gamma, phi = phi,
+                                  lower = min(x), upper = max(x))
+
+tau_k_dr <- root_tau_k_dr(x, a = a, gamma = gamma, phi = NA, A = A, k = k)
+roots_k_dr <- rootSolve::uniroot.all(f=root_tau_k_dr,
+                                     a = a, gamma = gamma, A = A, k = k,
+                                     lower = min(x), upper = max(x))
+
+plot(x, tau_k_dr, type='l', xlab='period/h', ylab='root_tau_k_dr')
+abline(h = 0, col=2, lty=2)
+abline(v = roots_k_dr, col=2, lty=2)
+
+plot(x, tau_k, type='l', xlab='period/h', ylab='root_tau_k_dr')
+abline(h = 0, col=2, lty=2)
+abline(v = roots_k, col=2, lty=2)
+
+
+## TODO: trace NA cases?
